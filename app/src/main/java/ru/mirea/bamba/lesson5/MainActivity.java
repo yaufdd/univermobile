1729b9f0740e5c65be0594e323161adb96dfd5cb
yaufdd <ericozavr@gmail.com>
2023-05-11 19:10:47 +0300

    first commit of lesson5

---
+++ app/src/main/java/ru/mirea/bamba/lesson5/MainActivity.java
@@ -0,0 +1,61 @@
+package ru.mirea.bamba.lesson5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import ru.mirea.bamba.lesson5.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding	= ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        ListView listSensor	= binding.listView;
+
+        ArrayList<HashMap<String, Object>> arrayList = new ArrayList<>();
+        for (int i = 0; i < sensors.size(); i++) {
+            HashMap<String, Object> sensorTypeList = new HashMap<>();
+            sensorTypeList.put("Name", sensors.get(i).getName());
+            sensorTypeList.put("Value", sensors.get(i).getMaximumRange());
+            arrayList.add(sensorTypeList);
+        }
+
+        SimpleAdapter mHistory =
+                new SimpleAdapter(this, arrayList, android.R.layout.simple_list_item_2,
+                        new String[]{"Name", "Value"},
+                        new int[]{android.R.id.text1, android.R.id.text2});
+        listSensor.setAdapter(mHistory);
+
+        Sensor accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, accelerometerSensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float valueAzimuth = event.values[0];
+            float valuePitch = event.values[1];
+            float valueRoll = event.values[2];
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
---
+++ audiorecord/src/main/java/ru/mirea/bamba/audiorecord/MainActivity.java
@@ -0,0 +1,135 @@
+package ru.mirea.bamba.audiorecord;
+
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
+
+import ru.mirea.bamba.audiorecord.databinding.ActivityMainBinding;
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private static final int REQUEST_CODE_PERMISSION = 200;
+    private final String TAG = MainActivity.class.getSimpleName();
+    private boolean isWork;
+    private String fileName = null;
+    private Button recordButton = null;
+    private Button playButton = null;
+    private MediaRecorder recorder = null;
+    private MediaPlayer player = null;
+    boolean isStartRecording = true;
+    boolean isStartPlaying = true;
+    String recordFilePath;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        recordButton = binding.recordButton;
+        playButton = binding.playButton;
+        playButton.setEnabled(false);
+        recordFilePath = (new File(getExternalFilesDir(Environment.DIRECTORY_MUSIC),
+                "/audiorecordtest.3gp")).getAbsolutePath();
+
+        int audioRecordPermissionStatus = ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.RECORD_AUDIO);
+        int storagePermissionStatus = ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE);
+        if (audioRecordPermissionStatus == PackageManager.PERMISSION_GRANTED && storagePermissionStatus
+                == PackageManager.PERMISSION_GRANTED) {
+            isWork = true;
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.RECORD_AUDIO,
+                    WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSION);
+        }
+
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isStartRecording) {
+                    recordButton.setText("Stop recording");
+                    playButton.setEnabled(false);
+                    startRecording();
+                } else {
+                    recordButton.setText("Start recording");
+                    playButton.setEnabled(true);
+                    stopRecording();
+                }
+                isStartRecording = !isStartRecording;
+            }
+        });
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isStartPlaying) {
+                    playButton.setText("Stop playing");
+                    recordButton.setEnabled(false);
+                    startPlaying();
+                } else {
+                    playButton.setText("Start playing");
+                    recordButton.setEnabled(false);
+                    stopPlaying();
+                }
+                isStartPlaying = !isStartPlaying;
+            }
+        });
+    }
+
+    private void startRecording() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setOutputFile(recordFilePath);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            Log.e(TAG, "prepare() failed");
+        }
+        recorder.start();
+    }
+
+    private void stopRecording() {
+        recorder.stop();
+        recorder.release();
+        recorder = null;
+    }
+
+    private void startPlaying() {
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(recordFilePath);
+            player.prepare();
+            player.start();
+        } catch (IOException e) {
+            Log.e(TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        player.release();
+        player = null;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_CODE_PERMISSION:
+                isWork = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!isWork ) finish();
+    }
+}
